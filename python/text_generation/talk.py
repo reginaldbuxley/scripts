e80bd4b07f1dda191e30b4cb5a81348f3eadfb0f
graphix <reginald.buxley@gmail.com>
2016-12-10 02:15:46 -0500

    work on seuss

--- python/text_generation/talk.py
+++ python/text_generation/talk.py
@@ -15,7 +15,7 @@ def doTheyRhyme(text1, text2):
         return False
 
 def getLastWord(text1):
-    return str.lower(re.sub('[^a-zA-Z0-9\n]', '', text1.rsplit(None, 1)[-1]))
+    return str.lower(re.sub('[^a-zA-Z0-9\n\']', '', text1.rsplit(None, 1)[-1]))
     
 
 def readFile(path):
@@ -32,44 +32,49 @@ def getShortSentence(text_model, length):
         
         if (sent is not None):
             rhymes = pronouncing.rhymes(getLastWord(sent))
-            if (len(rhymes) < 4):
+            if (len(rhymes) < 2):
                 sent = None
 
     return(sent)
 
-def getSuessSentence(text_model):
-    print ("Into the Suess")
+def getSuessSentence(text_model, length):
     #generate a sentence
     sent1 = None
     sent2 = None
 
-    while sent1 is None:
+    while sent2 is None:
+        print (".",end="",flush=True)
         #sent1 = text_model.make_sentence()
-        sent1 = getShortSentence(text_model,140)
-        last_word = getLastWord(sent1)
-        #print (last_word)
-        rhymes = pronouncing.rhymes(last_word)
-        
-        if len(rhymes) > 0 and sent1 is not None:
-            for rhyme in rhymes:
-                try:
-                    #print(rhyme)
-                    sent2 = text_model.make_sentence_with_start(beginning=rhyme)
-                    if sent2 is not None:
-                        print (sent1 + sent2)
-                except Exception as e:
-                    pass
-                    #print (str(e))
-                    sent1 = None 
+        sent1 = getShortSentence(text_model,length)
+
+        if sent1 is not None:
+            last_word = getLastWord(sent1)
+            
+            #last_word = last_word.capitalize()
+            rhymes = pronouncing.rhymes(last_word)
+
+            if len(rhymes) > 2 and rhymes is not None: 
+                for rhyme in rhymes:
+                    try:
+                        x = str(rhyme).capitalize()
+                        sent2 = text_model.make_sentence_with_start(beginning=x)
+                    except Exception as e:
+                        #print ("e = " + str(e))
+                        continue
+            else:
                     sent2 = None
+                        
 
         else:
             sent1 = None
             sent2 = None
-                
+
+            
+    print()              
+    sent2 = re.sub('___BEGIN__ ','',sent2)
+    return(str(sent1)+" "+str(sent2))
 
 
-    return(sent1+" "+sent2)
     #return (sent1 + " " + sent2)
 
 
@@ -101,28 +106,44 @@ print (len(text1))
 print (len(text2))
 print (len(text3))
 
-text_model1 = markovify.Text(text1,state_size=3)
-text_model2 = markovify.Text(text2,state_size=3)
-text_model3 = markovify.Text(text3,state_size=3)
+text_model1 = markovify.Text(text1,state_size=1)
+text_model2 = markovify.Text(text2,state_size=1)
+text_model3 = markovify.Text(text3,state_size=1)
 
+    #combined text models do not seem to work correctly
+    #throws : unorderable types: int() < NoneType()
+#text_model = markovify.combine([ model_a, model_b ], [ 1.5, 1 ])
+#text_model = markovify.combine([ text_model1, text_model2],[1,2])
 
-
-text_model = markovify.combine([text_model1, text_model2, text_model3],[1,1,1])
-
+text_model = markovify.Text(text1+text2+text3, state_size=3)
 sent1 = None 
 sent2 = None
 print ("++")
 
-while sent1 is None and sent2 is None:
-    sent1 = getShortSentence(text_model,140)
-    sent2 = getShortSentence(text_model,140)
+sSent = None
+
+for i in range(3):
+    while sSent is None:
+        sSent = getSuessSentence(text_model,80)
+    print (sSent)
+    sSent = None
+
+
+
+
+
+#while sent1 is None and sent2 is None:
+while 1 == 2:
+    sent1 = getShortSentence(text_model,35)
+    sent2 = getShortSentence(text_model,35)
 
     if doTheyRhyme(sent1,sent2):
             print (sent1 + " " + sent2)
     else:
-        print ("nope :-(")
+        print (".",end="",flush=True)
         sent1 = None
         sent2 = None
+
 
 
 
