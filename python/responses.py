53607c280e9670530903dd5ebba399d0a3923bd4
reginaldbuxley <reginald.buxley@gmail.com>
2021-10-19 18:42:56 -0400

    Create responses.py

---
+++ python/responses.py
@@ -0,0 +1,8 @@
+# This might be usable in other languages 
+# depending on syntax.
+
+SURPRISE = ['!!!!!','!!','GOOD GRAVY!','WOWZA','WHOA!',
+            'HUUHP!','RIDE ON!', 'WOW!','BLIMEY!!','BA-ZINGO!',
+            'GREAT GREEK GRAPES BATMAN!','PRESTO SMESTO!',
+            'BINGO BANGO TINGO TANGO']
+
